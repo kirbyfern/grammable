8d325b5d661faa1f0397ff851c4d1e03983f3961
kboygit <heybisayangdako@gmail.com>
2017-04-27 23:33:59 +0000

    completed CRUD actions, perform test on destroy action

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,14 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe 'grams#destroy action' do
+    it 'should allow a user to destroy grams' do
+      gram = FactoryGirl.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it 'should return a 404 message if we cannot find a gram with the id that is specified' do
+      delete :destroy, params: { id: 'SPACEDUCK' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe 'grams#update action' do
     it 'should allow users to successfully update grams' do
       gram = FactoryGirl.create(:gram, message: "Initial Value")
