d29d0a80d3b91ca284d3690ecae96b1579fbab22
kboygit <heybisayangdako@gmail.com>
2017-04-29 05:56:04 +0000

    added validations on the picture field, used fixtures to populate an image test

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,7 @@ FactoryGirl.define do
 
   factory :gram do
     message "hello"
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
     association :user
   end
 end
